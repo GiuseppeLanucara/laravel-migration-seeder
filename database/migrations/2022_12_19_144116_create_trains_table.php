00afdb37703db17163b98dbf4925cec4e69c5e21
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-12-19 15:53:32 +0100

    migration done

--- database/migrations/2022_12_19_144116_create_trains_table.php
+++ database/migrations/2022_12_19_144116_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 10);
+            $table->smallInteger('total_carriages_num')->unsigned()->nullable();
+            $table->boolean('in_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
