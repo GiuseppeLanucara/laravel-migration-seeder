edaca79f544a4aaf7dbf024201c0464e3c25380a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-12-20 16:29:37 +0100

    sedder error

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +13,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();   //
+            $train->azienda = $faker->word();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->dateTimeBetween('+0 days', '+7 days');
+            $train->orario_arrivo = $faker->dateTimeBetween('+0 days', '+7 days');
+            $train->codice_carrozza = $faker->bothify("??######");
+            $train->carrozze = $faker->numberBetween(1, 15);
+            $train->in_orario = $faker->numberBetween(0, 1);
+            $train->cancellato = $faker->numberBetween(0, 1);
+            $train->save();
+        }
     }
 }
