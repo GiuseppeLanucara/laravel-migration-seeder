7b6e708cc91703c48c4b19b4a558f2fa3d3119d1
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-12-20 16:56:38 +0100

    esercizio svolto

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Train;
+
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 
@@ -17,15 +18,15 @@ class TrainsTableSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $train = new Train();   //
-            $train->azienda = $faker->word();
-            $train->stazione_partenza = $faker->city();
-            $train->stazione_arrivo = $faker->city();
-            $train->orario_partenza = $faker->dateTimeBetween('+0 days', '+7 days');
-            $train->orario_arrivo = $faker->dateTimeBetween('+0 days', '+7 days');
-            $train->codice_carrozza = $faker->bothify("??######");
-            $train->carrozze = $faker->numberBetween(1, 15);
-            $train->in_orario = $faker->numberBetween(0, 1);
-            $train->cancellato = $faker->numberBetween(0, 1);
+            $train->company = $faker->word();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->dateTimeBetween('+0 days', '+7 days');
+            $train->arrival_time = $faker->dateTimeBetween('+0 days', '+7 days');
+            $train->train_code = $faker->bothify("??######");
+            $train->total_carriages_num = $faker->numberBetween(1, 15);
+            $train->in_time = $faker->numberBetween(0, 1);
+            $train->cancelled = $faker->numberBetween(0, 1);
             $train->save();
         }
     }
